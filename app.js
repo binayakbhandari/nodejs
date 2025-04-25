b1529bf5870aa2f3ad999ebbc83a276917dea2f2
binayakbhandari <binayakbhandari60@gmail.com>
2025-04-25 07:13:12 +0545

    Complted Middleware

--- app.js
+++ app.js
@@ -2,24 +2,32 @@ require('dotenv').config()
 const express = require('express')
 const connectToDatabase = require('./database')
 const Blog = require('./model/blogModel')
+const { multer, storage } = require('./middleware/multerConfig')
+const upload = multer({storage: storage})
+const fs = require('fs')
 const app = express()
 app.use(express.json())
 
 
-
+storage
 connectToDatabase()
 
  // API to create blog
-app.post("/blog", async (req, res)=>{
+app.post("/blog", upload.single('image'), async (req, res)=>{
     const {title, subtitle, description} = req.body
-    await Blog.create({
+    const {filename} =  req.file
+    console.log(filename)
+    const blog = await Blog.create({
         title : title,
         subtitle : subtitle,
-        description : description
+        description : description,
+        image : filename
     })
     res.status(200).json({
         message : "Blog Created Successfully !",
+        data : blog
     })
+    
 })
 
 // API to fetch blogs
