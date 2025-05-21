29ce21f7fe5be593342c00a1af8cf98eec89bf5c
binayakbhandari <binayakbhandari60@gmail.com>
2025-05-21 18:24:16 +0545

    Project Live

---
+++ model/blogModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const blogSchema =  new mongoose.Schema({
+    title : {
+        type : String
+    },
+    subtitle : {
+        type : String
+    },
+    description : {
+        type : String
+    },
+    image : {
+        type: String
+    }
+
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+module.exports = Blog
